5edc1209a4eaaeeceea482f1b3ca2b85392e1041
MuhammadSubhanZia <subhanzia051@gmail.com>
2025-07-06 03:50:09 +0500

    some bug fixed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// Add any additional plugins or configurations as needed
+// If you are using a custom plugin, make sure to include it here   
